13ae6e0399e9a1a93a37ea04f03f2f8c04e74797
ADEsposito <a.desposito640@gmail.com>
2017-01-04 19:34:47 -0600

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
+
+/* create variables for each card, checked for equality/inequality in Chrome console*/
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
