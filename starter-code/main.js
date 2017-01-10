343d35dd4a1ac0b19f8f31724f8ebe1966c4182e
ADEsposito <a.desposito640@gmail.com>
2017-01-10 15:04:31 -0600

    add conditional statement checking equality between cardTwo and cardFour, as well as alert notifying user of result of that check

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,11 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+/* create if...else statement testing for equality between cardTwo and cardFour, and alert notifying user if equality is found/not found */
+if (cardTwo === cardFour) {
+  alert('You found a match!');
+}
+else {
+  alert('Sorry, try again.');
+}
