425038dbe1734448689d043ef83b103d3a5e6158
ADEsposito <a.desposito640@gmail.com>
2017-01-19 18:43:00 -0600

    modify HTML file so cards can be generated with JS, create HTML for the cards using JS, add function in JS to createCards

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,10 +6,23 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-/* create if...else statement testing for equality between cardTwo and cardFour, and alert notifying user if equality is found/not found */
+/* Comment out per directions in Unit 10
 if (cardTwo === cardFour) {
   alert('You found a match!');
 }
 else {
   alert('Sorry, try again.');
-}
+} */
+
+/* get an element with the id of "game-board" and set it to a variable; create a for loop that creates an HTML element for each card (each should be a div, and have the class "card"); append each new card to the div with the id "game-board" */
+var deck = 4;
+
+var createCards = function() {
+  for (var i = 0; i < deck; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    document.getElementById('game-board').appendChild(newCard);
+  }
+};
+
+createCards();
